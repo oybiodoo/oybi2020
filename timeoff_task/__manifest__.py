cc1aaaeb125f998f4edb98d537401c1836a3c6bb
oybiodoo <61978207+oybiodoo@users.noreply.github.com>
2021-05-27 10:58:47 -0400

    Add files via upload

---
+++ timeoff_task/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Time Off Task",
+
+    'summary': """
+        This module is developed to handle the functionality of adding
+        a computed field over the Time Off view.""",
+
+    'description': """
+        This module is developed to handle the functionality of adding
+        a computed field over the Time Off view.
+    """,
+
+    'author': "Fazal Ur Rahman",
+    'category': 'Extra Tools',
+    'version': '13.0.0.1',
+
+    'depends': ['base', 'hr'],
+
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
